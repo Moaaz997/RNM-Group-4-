a9231536b7f1473dde3f7186e472c64a28209930
batouldaou <105592545+batouldaou@users.noreply.github.com>
2022-05-16 13:05:25 +0200

    Add files via upload

--- panda_joint_reader_node.cpp
+++ panda_joint_reader_node.cpp
@@ -1,116 +1,102 @@
 //
-// Created by rnm on 05.05.22.
-// Subscribe to the /jointstates_topic
+// Created by sherif on 15.05.22.
+//
+
 #include "ros/ros.h"
 #include "sensor_msgs/JointState.h"
 #include "math.h"
-
-
-double** multiply (double *A , double *B) {
-    double**  multi;
-    //int row = sizeof(A)/sizeof (A[0]);
-    int row = 4;
-    //int col = sizeof(A[0])/sizeof(int);
-    int col = 4;
-    for (int i=0; i<row; ++i) {
-        for (int j=0; j<col; ++j){
-            for (int k=0; k<col; ++k ){
-
-                //multi += A[i][k] * B[k][j] ;
-            }
-
-        }
-    }
-    return multi;
-}
+#include <Eigen/Dense>
+#include <iostream>
+using namespace Eigen;
+using namespace std;
 
 void transformationMatrix (double* joint) {
-    double A1[4][4];
-    double A2[4][4];
-    double A3[4][4];
-    double A4[4][4];
-    double A5[4][4];
-    double A6[4][4];
-    double A7[4][4];
+    MatrixXf A1(4,4);
+    MatrixXf A2(4,4);
+    MatrixXf A3(4,4);
+    MatrixXf A4(4,4);
+    MatrixXf A5(4,4);
+    MatrixXf A6(4,4);
+    MatrixXf A7(4,4);
+    MatrixXf A8(4,4);
+
 
     for (int i=0; i<7 ; i++ ) {
         switch (i) {
             case '0':
-                A1= {{cos(joint[i]),-sin (joint[i]),0, 0},
-                {sin(joint[i]), cos (joint[i]), 0, 0},
-                {0, 0, 1, 0.333},
-                {0, 0, 0, 1}};
+                A1 << cos(joint[i]),-sin (joint[i]),0, 0,
+                     sin(joint[i]), cos (joint[i]), 0, 0,
+                     0, 0, 1, 0.333,
+                     0, 0, 0, 1;
                 break;
 
             case '1':
-                A2 = {{cos(joint[i]),0, -sin (joint[i]), 0},
-                {sin(joint[i]), 0, cos (joint[i]),0},
-                {0, -1, 0, 0},
-                {0, 0, 0, 1}};
+                A2 << cos(joint[i]),0, -sin (joint[i]), 0,
+                      sin(joint[i]), 0, cos (joint[i]),0,
+                      0, -1, 0, 0,
+                      0, 0, 0, 1;
                 break;
 
             case '2':
-                 A3=  {{cos(joint[i]),0, sin (joint[i]), 0},
-                       {sin(joint[i]), 0, -cos (joint[i]), 0},
-                       {0, 1, 0, 0.316},
-                       {0, 0, 0, 1}};
+                A3 << cos(joint[i]),0, sin (joint[i]), 0,
+                      sin(joint[i]), 0, -cos (joint[i]), 0,
+                      0, 1, 0, 0.316,
+                      0, 0, 0, 1;
                 break;
 
             case '3':
-                A4  =  {{cos(joint[i]),0, sin (joint[i]), 0.0825*cos(joint[i])},
-                        {sin(joint[i]), 0, -cos (joint[i]), 0.0825*cos(joint[i])},
-                        {0, 1, 0, 0},
-                        {0, 0, 0, 1}};
+                A4  << cos(joint[i]),0, sin (joint[i]), 0.0825*cos(joint[i]),
+                        sin(joint[i]), 0, -cos (joint[i]), 0.0825*cos(joint[i]),
+                        0, 1, 0, 0,
+                        0, 0, 0, 1;
                 break;
 
             case '4':
-                A5 =  {{cos(joint[i]),0, -sin (joint[i]), -0.0825*cos(joint[i])},
-                {sin(joint[i]), 0, cos (joint[i]), -0.0825*cos(joint[i])},
-                {0, -1, 0, 0},
-                {0, 0, 0, 1}};
+                A5 << cos(joint[i]),0, -sin (joint[i]), -0.0825*cos(joint[i]),
+                       sin(joint[i]), 0, cos (joint[i]), -0.0825*cos(joint[i]),
+                       0, -1, 0, 0,
+                       0, 0, 0, 1;
                 break;
 
             case '5':
-                A6 =  {{cos(joint[i]),0, sin (joint[i]), 0},
-                {sin(joint[i]), 0, -cos (joint[i]), 0},
-                {0, 1, 0, 0},
-                {0, 0, 0, 1}};
+                A6 << cos(joint[i]),0, sin (joint[i]), 0,
+                       sin(joint[i]), 0, -cos (joint[i]), 0,
+                       0, 1, 0, 0,
+                       0, 0, 0, 1;
                 break;
             case '6':
-                A7 =  {{cos(joint[i]),0, sin (joint[i]), 0.088*cos(joint[i])},
-                {sin(joint[i]), 0, -cos (joint[i]), 0.088*cos(joint[i])},
-                {0, 1, 0, 0},
-                {0, 0, 0, 1}};
+                A7 << cos(joint[i]),0, sin (joint[i]), 0.088*cos(joint[i]),
+                       sin(joint[i]), 0, -cos (joint[i]), 0.088*cos(joint[i]),
+                       0, 1, 0, 0,
+                       0, 0, 0, 1;
                 break;
 
         }
 
     }
-    double T20[][] = multiply (A1,A2);
-    double T42[][] = multiply (A3,A4);
-    double T64[][]= multiply (A5,A6);
-    double T74[][] = mutliply (T64,A7);
-    double T40[][] = multiply (T20,T42);
-    double T70[][] = multpily (T40,T74);
+    MatrixXf T70 (4,4);
+    T70 << A1*A2*A3*A4*A5*A6*A7 ;
+    ROS_INFO_STREAM(T70);
+
 }
 
 void callBack (const sensor_msgs::JointState& msg)
 {
     double position[6];
-   
+
     std::cout << "Joints: ";
     for (int i=0; i<7 ; ++i ) {
         position[i] = msg.position[i];
         std:: cout << position[i] << " " ;
-        }
+    }
     transformationMatrix (position);
     std::cout << "/n/n";
-    }
+}
 
-    int main(int argc, char **argv)
+int main(int argc, char **argv)
 {
     ros::init(argc, argv, "panda_joint_reader_node");
-    ros::NodeHandle nh(~);
+    ros::NodeHandle nh;
 
     std::string topic_name;
     int queue_size;
@@ -121,4 +107,3 @@ void callBack (const sensor_msgs::JointState& msg)
 
     return 0;
 }
-
